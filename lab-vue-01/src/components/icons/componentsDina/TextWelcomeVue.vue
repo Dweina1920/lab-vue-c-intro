28f78ea1c3b6cb66b92a1571535ca0f465355420
Dina <dina.yamanid@gmail.com>
2023-01-30 21:18:18 +0100

    text welcome and button

--- lab-vue-01/src/components/icons/componentsDina/TextWelcomeVue.vue
+++ lab-vue-01/src/components/icons/componentsDina/TextWelcomeVue.vue
@@ -1,19 +1,18 @@
-
-
 <template>
-    <div class="container-text-welcome-vue">
-        <p class="big-title">Welcome to Vue.js</p>
-    </div>
+  <div class="container-text-welcome-vue">
+    <p class="big-title">Welcome to Vue.js</p>
+    <p class="little-title">
+      Becoming a coding ninja with the fastest-<br />growing JavaScript
+      framework
+    </p>
+  </div>
+  <div class="div-button-welcome">
+    <button class="button-welcome">Awesome!</button>
+  </div>
 </template>
-
 
 <script setup></script>
 
-
 <style scoped>
-.big-title{
-    font-size: 60px;
-    font-weight: 800;
-    text-align: center;
-}
+
 </style>
