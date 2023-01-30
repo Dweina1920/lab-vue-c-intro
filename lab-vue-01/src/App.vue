fad01217f655401a3fa2a40bd5ec2cc0786726e6
Dina <dina.yamanid@gmail.com>
2023-01-30 17:53:44 +0100

    first-commit

---
+++ lab-vue-01/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HeaderComponent from "./components/icons/componentsDina/HeaderComponent.vue";
+import DivImageVue from "./components/icons/componentsDina/DivImageVue.vue";
+</script>
+
+<template>
+  <div>
+    <HeaderComponent />
+  </div>
+
+  <main>
+    <DivImageVue/>
+  </main>
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ lab-vue-01/src/components/icons/componentsDina/HeaderComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header">
+    <img
+      class="logo-ironhack"
+      src="../../../img/ironhack-logo-negro1-removebg-preview.png"
+    />
+    <div class="div-hamburguer-menu">
+    <img src = "../../../img/images-removebg-preview.png" alt="hamburguer-menu" class="hambuerguer-menu"/>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+@import url(https://fonts.googleapis.com/css?family=Raleway);
+
+.logo-ironhack {
+  width: 5%;
+  height: 5%;
+}
+.header{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 90%;
+    margin: 0 auto;
+    margin-top: 2%;
+}
+
+.div-hamburguer-menu{
+    
+    width: 5%;
+    height: 7%;
+}
+.hambuerguer-menu{
+width: 100%;
+height: 100%;
+}
+</style>
